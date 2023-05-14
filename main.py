718abdfa3832ad854d6f819e25c3eeb604fec9e0
khalid <10abha10@gmail.com>
2023-05-14 13:15:46 +0300

    solved lab

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ def validate_numbers(func):
         list1=[]
        
         for i in range(len(args)):
-          if type(args[i])!=(int or float) or args[i]<0:
+          if type(args[i])!=int and type(args[i])!=float or args[i]<0:
                 raise Exception("the value shoud be non-negative number")
         for i in kwargs:
-            if type(kwargs[i])!=(int or float) or kwargs[i]<0:
+            if type(kwargs[i])!=int and type(kwargs[i])!=float or kwargs[i]<0:
                 raise Exception("the value shoud be non-negative number")
 
 
@@ -18,11 +18,11 @@ def validate_numbers(func):
 def add_values(x,y):
     print(x+y)
 
-
+    
 @validate_numbers    
 def multiply_value(x,y):
     print(x*y)
 
 
-add_values(1,y=-6)
-multiply_value(4,4)
+add_values(1,y=-6.5)
+multiply_value(4,4.5)
